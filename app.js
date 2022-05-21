e58a8011f24cb0b885f68cd0a6e1d4acf36b578d
dinosauce-285 <thanhbualqt@gmail.com>
2022-05-21 15:28:07 +0700

    done api

--- app.js
+++ app.js
@@ -22,22 +22,66 @@ function createAccount() {
 regisBtn.onclick = createAccount;
 let newUserName = document.getElementById("createUserName");
 let newPassword = document.getElementById("createPassword");
+let address = document.getElementById("address");
+let phoneNum = document.getElementById("phoneNumber");
 let signUpBtn = document.getElementById("signUpBtn");
+
 function regisAccountList() {
   let accountContainer = [];
   let account = {
     name: newUserName.value,
     password: newPassword.value,
-  }
+    address: address.value,
+    phone: phoneNum.value,
+  };
 
   accountContainer = JSON.parse(localStorage.getItem("users"));
 
   if (accountContainer == null) {
     accountContainer = [];
   }
-accountContainer.push(account)
+  accountContainer.push(account);
   let stringInfor = JSON.stringify(accountContainer);
   localStorage.setItem("users", stringInfor);
+  main.style.opacity = "1";
+  container2.style.visibility = "hidden";
+  container2.style.opacity = "0";
+  container.style.visibility = "hidden";
+  container.style.opacity = "0";
 }
 
 signUpBtn.onclick = regisAccountList;
+let intUserName = document.getElementById("insertUsername");
+let intPassword = document.getElementById("insertPassword");
+let logInBtn = document.getElementById("logInBtn");
+
+function findAccount() {
+  let accountData = JSON.parse(localStorage.getItem("users"));
+  for (let i = 0; i < accountData.length; i++) {
+    if (accountData[i].name == intUserName.value) {
+      accountIn = accountData[i];
+    }
+  }
+
+  if (accountIn.password == intPassword.value) {
+    main.style.opacity = "1";
+    container.style.visibility = "hidden";
+    container.style.opacity = "0";
+  }
+  if (accountIn.password != intPassword.value) {
+    let wrong = document.getElementById("wrong");
+    wrong.innerHTML = `<div>Bạn đã nhập sai mật khẩu</div>`;
+  }
+   
+}
+
+logInBtn.onclick = findAccount;
+async function showPic() {
+ const apiLink = await fetch('https://foodish-api.herokuapp.com/api')
+ let picLink =  await apiLink.json() ;
+
+ let container = document.getElementById('banner')
+
+container.innerHTML = `<img src="${picLink.image}" alt="">`
+}
+showPic()
